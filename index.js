bc3a1c6e074f1decd806d4eece4400008209c184
vipul-8840 <vipulkumarlen@gmail.com>
2025-01-25 23:43:21 +0530

    action by payload

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const store = createStore(reducer,applyMiddleware(logger.default));
 
 function reducer (state ={amount:1},action)
 {
-    if(action.type==='increment')
+    // if(action.type==='increment')
+    // {
+    //     return {amount:state.amount+1};
+    // }
+
+    if(action.type==='incrementByvipul')
     {
-        return {amount:state.amount+1};
+        return {amount:state.amount+ action.payload};
     }
         return state;
 }
 
 
+// setInterval(()=>{
+//     store.dispatch({type:'increment'});
+// },3000)
+
 setInterval(()=>{
-    store.dispatch({type:'increment'});
+    store.dispatch({type:'incrementByvipul',payload:4});
 },3000)
