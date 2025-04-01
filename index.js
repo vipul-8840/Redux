1e57252714cbc5605f8aa59b35c0f6bf447637bd
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-01 21:35:15 +0530

    first reducer function with action

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 
-import {createStore} from 'redux'
+// import {createStore} from 'redux'
 
-console.log("hello world")
+//console.log("hello world")
 //global store
-const store = createStore(reducer);
+// const store = createStore(reducer);
 //reducer
-function reducer(state={amount:1},action)
-{
-    return state 
-}
+// function reducer(state={amount:1},action)
+// {
+//     return state 
+// }
 
-console.log(store.getState())
+// console.log(store.getState())
+
+import {createStore} from 'redux'
+const store = createStore(reducer);
+function reducer(state={amount:1},action){
+    if(action.type=='increment')
+    {
+        return {amount:state.amount+1};
+    }
+      return state;
+}
+console.log(store.getState());
+store.dispatch({type:'increment'});
+console.log(store.getState());
