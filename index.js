62ac7413a574592403ed2f4f8e74da061fc972c2
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-01 22:17:59 +0530

    apply middleware in redux

--- index.js
+++ index.js
@@ -12,15 +12,73 @@
 
 // console.log(store.getState())
 
-import {createStore} from 'redux'
-const store = createStore(reducer);
-function reducer(state={amount:1},action){
+// import {createStore} from 'redux'
+// const store = createStore(reducer);
+// function reducer(state={amount:1},action){
+//     if(action.type=='increment')
+//     {
+//         return {amount:state.amount+1};
+//     }
+//       return state;
+// }
+// console.log(store.getState());
+// store.dispatch({type:'increment'});
+// console.log(store.getState());
+
+// import {createStore} from 'redux';
+// const store = createStore(reducer);
+// const history=[]
+// function reducer(state={amount:1},action)
+// {
+//     if(action.type=='increment')
+//     {
+//         // wrong way doing this 
+//         state.amount=state.amount+1;
+//     }
+//     return state;
+// }
+
+// store.subscribe(()=>{
+//     history.push(store.getState());
+//    console.log(history)
+// })
+
+// setInterval(()=>{
+    
+//     store.dispatch({type:'increment'})
+ 
+
+// },2000)
+
+// import {createStore} from 'redux'
+// const store = createStore(reducer);
+// const history=[]
+// function reducer(state={amount:1},action)
+// {
+//     if(action.type=='increment')
+//         return {amount:state.amount+1};
+//     return state;
+// }
+
+// store.subscribe(()=>{
+//     history.push(store.getState());
+//     console.log(history)
+// })
+// setInterval(()=>{
+//     store.dispatch({type:'increment'});
+// },2000)
+
+import {createStore,applyMiddleware} from 'redux';
+import logger from 'redux-logger'
+const store = createStore(reducer,applyMiddleware(logger.default));
+function reducer(state={amount:1},action)
+{
     if(action.type=='increment')
     {
-        return {amount:state.amount+1};
+        return {amount:state.amount+1}
     }
-      return state;
+    return state;
 }
-console.log(store.getState());
-store.dispatch({type:'increment'});
-console.log(store.getState());
+setInterval(()=>{
+   store.dispatch({type:'increment'})
+},5000)
